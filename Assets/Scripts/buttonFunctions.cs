76238c42ac684bda39071719eb1924dd2b00328a
Bradley Hulbert <Bradleyhulbert@aol.com>
2022-11-01 15:32:53 -0400

    buttonFunction Changes
    Added the buttonFunction script

---
+++ Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    public void resume()
+    {
+        gameManager.instance.unpause();
+        gameManager.instance.pauseMenu.SetActive(false);
+        gameManager.instance.isPaused = false;
+    }
+    public void restart()
+    {
+        gameManager.instance.unpause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+    //public void respawn()
+    //{
+    //    gameManager.instance.unpause();
+    //    gameManager.instance.playerScript.respawn();
+    //}
+}
