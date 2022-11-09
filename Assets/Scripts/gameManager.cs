e57240dc8b6dc7cba909a058510078aa5627c2ec
Bradley Hulbert <Bradleyhulbert@aol.com>
2022-11-08 21:20:14 -0500

    fixed a bug with the pause menu
    fixed a bug where if you paused the screen while being in a powerup, you couldnt use your mouse. fixed by making sure you cant pause while in a powerup state.

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -37,7 +37,7 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Cancel") && !playerDeadMenu.activeSelf && !winMenu.activeSelf)
+        if (Input.GetButtonDown("Cancel") && !playerDeadMenu.activeSelf && !winMenu.activeSelf && !playerPowerupScreen.activeSelf)
         {
             isPaused = !isPaused;
             pauseMenu.SetActive(isPaused);
