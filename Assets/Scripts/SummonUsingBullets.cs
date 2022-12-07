ed33cd931620a378472b953fa45816a6f254e2d2
Bradley Hulbert <Bradleyhulbert@aol.com>
2022-12-07 18:03:17 -0500

    added ice effects
    ice will now fall down on enemy1 when hit, still a work in progress but after 3 shots they should freeze solid permanently.

--- Assets/Scripts/SummonUsingBullets.cs
+++ Assets/Scripts/SummonUsingBullets.cs
@@ -6,9 +6,21 @@ public class SummonUsingBullets : MonoBehaviour
 {
     public GameObject[] Fire;
     public GameObject[] Ice;
+    public GameObject frozen1;
+    public GameObject frozen2;
+    public GameObject frozen3;
     public GameObject[] Lightning;
     public GameObject[] Meteor;
+    public GameObject enemy;
+    int freeze;
+    Vector3 enemyPos;
     public int timer;
+
+    private void Update()
+    {
+        
+
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Fire"))
@@ -19,13 +31,41 @@ public class SummonUsingBullets : MonoBehaviour
             }
             
         }
+
         if (other.CompareTag("Ice"))
         {
-            for (int i = 0; i < Ice.Length; i++)
+            if (freeze == 3) // if the enemy is hit 3 times with an ice spell they freeze. this will disable the enemy and place a frozen 3d model of them instead.
             {
-                Ice[i].SetActive(true);
+                stuff();
+                
             }
+           
+        }
+        if (other.CompareTag("Ice"))
+        {
+            if(freeze >= 3) // if the enemy is hit 3 times with an ice spell they freeze. this will disable the enemy and place a frozen 3d model of them instead.
+            {
+                enemyPos = this.transform.position;
+                frozen1.transform.position = enemyPos;
+                frozen2.transform.position = enemyPos;
+                frozen3.transform.position = enemyPos;
+                frozen1.SetActive(true);
+                frozen2.SetActive(true);
+                frozen3.SetActive(true);
+                for (int i = 0; i < Ice.Length; i++)
+                {
+                    Ice[i].SetActive(false);
+                }
+                Destroy(enemy);
+            }
+            
+            if(freeze < 3)
+            {
+                Ice[freeze].SetActive(true); // this will increase the number of freeze effects each time they are hit. 
 
+                freeze += 1;
+            }
+            
         }
         if (other.CompareTag("Lightning"))
         {
@@ -44,6 +84,10 @@ public class SummonUsingBullets : MonoBehaviour
 
         }
 
+    }
+    public void stuff()
+    {
+        
     }
 
 
