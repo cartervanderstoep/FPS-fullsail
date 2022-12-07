080aaea561a5706107227f15775c76015ee41500
nodnarb-2003 <badennis07@gmail.com>
2022-12-07 14:41:56 -0500

    Meteor

--- Assets/Scripts/magicAttack.cs
+++ Assets/Scripts/magicAttack.cs
@@ -6,6 +6,7 @@ public class magicAttack : MonoBehaviour
 {
     [Header ("----- Components -----")]
     [SerializeField] Rigidbody magicRB;
+    [SerializeField] GameObject expl; 
     [SerializeField] int magicDamage;
     [Range(1, 5)][SerializeField] int magicElm; 
     [SerializeField] float magicSpeed;
@@ -19,24 +20,52 @@ public class magicAttack : MonoBehaviour
         {
             magicRB.velocity = transform.forward * magicSpeed;
         }
-        else if(magicElm == 3 || magicElm == 4)
+        else if(magicElm == 3)
         {
             magicRB.AddForce(Vector3.down * magicSpeed); 
         }
+        else if (magicElm == 4)
+        {
+            StartCoroutine(timer()); 
+        }
         Destroy(gameObject, attackTimer);
+    }
+    
+    IEnumerator timer()
+    {
+        yield return new WaitForSeconds(attackTimer - 0.5f);
+        Instantiate(expl, transform.position, transform.rotation); 
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy") /*&& Vector3.Distance(transform.position, other.transform.position) <= 4*/)
+        if (magicElm != 4)
         {
-            other.GetComponent<IDamage>().takeDamage(magicDamage);
-            Destroy(gameObject);
+            if (other.CompareTag("Enemy") /*&& Vector3.Distance(transform.position, other.transform.position) <= 4*/)
+            {
+                other.GetComponent<IDamage>().takeDamage(magicDamage);
+                Destroy(gameObject);
+            }
+            else if (!other.CompareTag("Enemy") && !other.CompareTag("enemy prefab"))
+            {
+                Destroy(gameObject);
+            }
         }
-        else if (!other.CompareTag("Enemy") && !other.CompareTag("enemy prefab")  )
+        else if(magicElm == 4)
         {
-            Destroy(gameObject);
+            if (other.CompareTag("Enemy") /*&& Vector3.Distance(transform.position, other.transform.position) <= 4*/)
+            {
+                other.GetComponent<IDamage>().takeDamage(magicDamage);
+                Instantiate(expl, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+            else if (!other.CompareTag("Enemy") && !other.CompareTag("enemy prefab"))
+            {
+                Instantiate(expl, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
         }
+        
 
     }
 }
