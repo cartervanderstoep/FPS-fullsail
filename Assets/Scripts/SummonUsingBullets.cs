bc16d0ffdd5c5850dfec395b79b7a9ff5959f51d
Bradley Hulbert <Bradleyhulbert@aol.com>
2022-12-09 04:34:49 -0500

    fixed some bugs
    fixed some bugs with the attack effects

--- Assets/Scripts/SummonUsingBullets.cs
+++ Assets/Scripts/SummonUsingBullets.cs
@@ -6,9 +6,7 @@ public class SummonUsingBullets : MonoBehaviour
 {
     public GameObject[] Fire;
     public GameObject[] Ice;
-    public GameObject frozen1;
-    public GameObject frozen2;
-    public GameObject frozen3;
+    public GameObject frozen;
     public GameObject[] Lightning;
     public GameObject[] Meteor;
     public GameObject enemy;
@@ -36,12 +34,10 @@ public class SummonUsingBullets : MonoBehaviour
             if(freeze >= 3) // if the enemy is hit 3 times with an ice spell they freeze. this will disable the enemy and place a frozen 3d model of them instead.
             {
                 enemyPos = this.transform.position;
-                frozen1.transform.position = enemyPos;
-                frozen2.transform.position = enemyPos;
-                frozen3.transform.position = enemyPos;
-                frozen1.SetActive(true);
-                frozen2.SetActive(true);
-                frozen3.SetActive(true);
+                //frozen.transform.position = enemyPos;
+                Instantiate(frozen, enemyPos, transform.rotation);
+                //frozen.SetActive(true);
+                
                 for (int i = 0; i < Ice.Length; i++)
                 {
                     Ice[i].SetActive(false);
